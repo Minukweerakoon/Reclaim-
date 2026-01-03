aeb3a64a58d53c09e1bf80be072ea4a22e5142f3
Nethmi Thambawita <it22911544@my.sliit.lk>
2026-01-03 11:31:25 +0530

    changes

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import UserModel
-from .device import DeviceModel
-
-__all__ = ['UserModel', 'DeviceModel']
--- backend/app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .auth import auth_bp
-from .devices import devices_bp
-from .alerts import alerts_bp
-
-__all__ = ['auth_bp', 'devices_bp', 'alerts_bp']
